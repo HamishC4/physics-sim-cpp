552fff5da54b311ec8e7c2c5ad4fc354fe0445d3
HamishC4 <hamishwcranston@gmail.com>
2026-05-19 18:15:28 +1000

    Add 3D visualisation of 2D array using mathplot. Experiment with mathplot

---
+++ build/CMakeFiles/PhysicsSimApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PhysicsSimApp.dir/src/mathplot-visualise.cpp.o"
+  "CMakeFiles/PhysicsSimApp.dir/src/mathplot-visualise.cpp.o.d"
+  "PhysicsSimApp"
+  "PhysicsSimApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PhysicsSimApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/PhysicsSimApp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PhysicsSimApp.
+# This may be replaced when dependencies are built.
